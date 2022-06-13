8adee0f4b69fc40c52b0e6ac757b8394af174bd8
Meypod <radical.zero@hotmail.com>
2022-06-13 21:02:31 +0430

    fix text direction issue

--- src/screens/settings/components/BatteryOptimizationSettings.tsx
+++ src/screens/settings/components/BatteryOptimizationSettings.tsx
@@ -43,11 +43,11 @@ export function BatteryOptimizationSettings(props: IStackProps) {
           flexDirection={
             isRTL ? 'row-reverse' : 'row'
           }>{t`Battery Optimization`}</FormControl.Label>
-        <FormControl.HelperText flexDirection={isRTL ? 'row-reverse' : 'row'}>
-          <Text fontSize="xs">
+        <FormControl.HelperText>
+          <Text textAlign={isRTL ? 'right' : 'left'} fontSize="xs">
             {t`Depending on your device and android version, Sometimes your device may kill the app in the background to save battery. This can prevent alarms/notifications from being delivered.`}
           </Text>
-          <Text fontSize="xs">
+          <Text textAlign={isRTL ? 'right' : 'left'} fontSize="xs">
             {t`If you want to make sure the app can work reliably, You have to disable power optimization for this app. You can open related settings using the button below.`}
           </Text>
           <HStack alignSelf="center">
@@ -70,15 +70,22 @@ export function BatteryOptimizationSettings(props: IStackProps) {
             flexDirection={
               isRTL ? 'row-reverse' : 'row'
             }>{t`Power Manager`}</FormControl.Label>
-          <FormControl.HelperText flexDirection={isRTL ? 'row-reverse' : 'row'}>
-            <Text>{t`Some devices need extra settings inside their Power Manager 
+          <FormControl.HelperText>
+            <Text
+              textAlign={
+                isRTL ? 'right' : 'left'
+              }>{t`Some devices need extra settings inside their Power Manager 
           to prevent the app from getting killed in the background.
           In that case disable any power saving option for the app.`}</Text>
-            <Text>{t`You can access your device Power Manager using the button below.`}</Text>
+            <Text
+              textAlign={
+                isRTL ? 'right' : 'left'
+              }>{t`You can access your device Power Manager using the button below.`}</Text>
 
             {powerManagerInfo?.activity &&
               powerManagerInfo.manufacturer?.toLowerCase() === 'samsung' && (
                 <Text
+                  textAlign={isRTL ? 'right' : 'left'}
                   py="3"
                   fontSize="xs">{t`Samsung devices have their own custom Power Manager called "Device Care".
           After pressing the button below, You will be navigated to the Battery menu of Device Care.
