9f87d2f4a68fc0fd5e294e8896df25fdee0e9f04
Meypod <radical.zero@hotmail.com>
2022-06-17 11:13:51 +0430

    move languages inside render

--- src/screens/settings/components/LanguageSettings.tsx
+++ src/screens/settings/components/LanguageSettings.tsx
@@ -2,39 +2,37 @@ import {t} from '@lingui/macro';
 import {HStack, FormControl, IStackProps, Select} from 'native-base';
 import {useEffect} from 'react';
 import {isRTL, loadLocale} from '@/i18n';
-import {settings, useSettingsHelper} from '@/store/settings';
+import {useSettingsHelper} from '@/store/settings';
 
 type LanguageEntry = {
   label: string;
   value: string;
 };
 
-const languageEntries: LanguageEntry[] = [
-  {
-    label: t`English`,
-    value: 'en',
-  },
-  {
-    label: t`Persian`,
-    value: 'fa',
-  },
-  {
-    label: t`Arabic`,
-    value: 'ar',
-  },
-];
-
-const userLang = settings.getState().SELECTED_LOCALE;
-
-if (!['en', 'fa', 'ar'].includes(userLang)) {
-  languageEntries.push({
-    label: userLang + ' (' + t`Unsupported` + ')',
-    value: userLang,
-  });
-}
-
 export function LanguageSettings(props: IStackProps) {
   const [lang, setLang] = useSettingsHelper('SELECTED_LOCALE');
+
+  const languageEntries: LanguageEntry[] = [
+    {
+      label: t`English`,
+      value: 'en',
+    },
+    {
+      label: t`Persian`,
+      value: 'fa',
+    },
+    {
+      label: t`Arabic`,
+      value: 'ar',
+    },
+  ];
+
+  if (!['en', 'fa', 'ar'].includes(lang)) {
+    languageEntries.push({
+      label: lang + ' (' + t`Unsupported` + ')',
+      value: lang,
+    });
+  }
 
   useEffect(() => {
     loadLocale(lang).catch(() => {});
