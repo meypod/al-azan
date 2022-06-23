014de4c706fd0f78c493ca2b43aa717b36160510
Meypod <radical.zero@hotmail.com>
2022-06-23 12:47:42 +0430

    stop adhan playback when it is interrupted

--- src/services/play_sound.ts
+++ src/services/play_sound.ts
@@ -14,21 +14,33 @@ export async function play(url: string | number) {
   });
   await TrackPlayer.updateOptions({
     stopWithApp: true,
-    alwaysPauseOnInterruption: true,
+    alwaysPauseOnInterruption: false,
   });
+  const interruptSub = TrackPlayer.addEventListener(
+    Event.RemoteDuck,
+    async () => {
+      // doesnt matter if `e.permanent` or `e.paused`, always stop playback
+      await TrackPlayer.stop();
+    },
+  );
   await TrackPlayer.setVolume(await SystemSetting.getVolume());
   await TrackPlayer.setRepeatMode(RepeatMode.Off);
 
   const playbackFinishedDefer = defer<void>();
-  const endSub = TrackPlayer.addEventListener(Event.PlaybackQueueEnded, () => {
-    endSub.remove();
+
+  const onFinally = () => {
+    interruptSub.remove();
     volumeListener.remove();
     playbackFinishedDefer.resolve();
+  };
+
+  const endSub = TrackPlayer.addEventListener(Event.PlaybackQueueEnded, () => {
+    endSub.remove();
+    onFinally();
   });
   const errorSub = TrackPlayer.addEventListener(Event.PlaybackError, err => {
     errorSub.remove();
-    volumeListener.remove();
-    playbackFinishedDefer.resolve();
+    onFinally();
     console.error('TrackPlayer Error: ', err);
   });
 
