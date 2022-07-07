158bc9e91edeb7974f0384a38202326475e5dec6
Meypod <radical.zero@hotmail.com>
2022-07-08 00:26:35 +0430

    add label icon

---
+++ src/assets/icons/label.tsx
@@ -0,0 +1,6 @@
+import {createIcon} from 'native-base';
+
+export const LabelIcon = createIcon({
+  viewBox: '0 0 48 48',
+  d: 'm42 24-8.45 11.95q-.65.9-1.55 1.475-.9.575-2 .575H9q-1.25 0-2.125-.875T6 35V13q0-1.25.875-2.125T9 10h21q1.1 0 2 .575.9.575 1.55 1.475Zm-3.75 0-7.7-11H9v22h21.55ZM9 24v11-22Z',
+});
