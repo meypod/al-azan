047575f9754878694857fe3133a5e3ab24f9c7d4
Meypod <radical.zero@hotmail.com>
2022-08-08 21:24:54 +0430

    change about page credits text a bit

--- src/screens/settings_about/index.tsx
+++ src/screens/settings_about/index.tsx
@@ -1,5 +1,11 @@
 import {t} from '@lingui/macro';
-import {ScrollView, IScrollViewProps, FormControl, Text} from 'native-base';
+import {
+  ScrollView,
+  IScrollViewProps,
+  FormControl,
+  Text,
+  Divider,
+} from 'native-base';
 import pkg from '@/../package.json';
 
 export function AboutSettings(props: IScrollViewProps) {
@@ -24,11 +30,16 @@ export function AboutSettings(props: IScrollViewProps) {
         <FormControl.Label m="0">{t`License`}:</FormControl.Label>
         <Text>AGPL-3.0</Text>
       </FormControl>
+      <Divider my="3"></Divider>
       <FormControl mb="3">
-        <FormControl.Label m="0">{t`Credits`}:</FormControl.Label>
-        <Text>{t`All copyrights for adhan voices belong to their respective owners.
-         Special thanks to translation.io (lingui.js) for their wonderful services.
-         Thanks to all open source community members who made all these wonderful libraries that made making this app possible.`}</Text>
+        <Text>
+          {t({
+            id: 'about.credits',
+            message: `All copyrights for adhan voices belong to their respective owners.
+            Special thanks to translation.io (lingui.js) for their wonderful services.
+            Thanks to all open source community members who made all these wonderful libraries that made making this app possible.`,
+          })}
+        </Text>
       </FormControl>
     </ScrollView>
   );
