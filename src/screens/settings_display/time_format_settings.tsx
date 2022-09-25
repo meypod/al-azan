d35f91f77e29e7f37fee61d3efe1ebe80467e888
Meypod <radical.zero@hotmail.com>
2022-09-25 11:06:42 +0330

    fix: remove unnecessary code from time format settings

--- src/screens/settings_display/time_format_settings.tsx
+++ src/screens/settings_display/time_format_settings.tsx
@@ -1,29 +1,9 @@
 import {t} from '@lingui/macro';
 import {HStack, FormControl, IStackProps, Switch, Text} from 'native-base';
-import {useEffect} from 'react';
-import {I18nManager} from 'react-native';
-import {isRTL, loadLocale} from '@/i18n';
-import {restart} from '@/modules/restart';
-import {settings, useSettingsHelper} from '@/store/settings';
+import {useSettingsHelper} from '@/store/settings';
 
 export function TimeFormatSettings(props: IStackProps) {
   const [is24Hour, setIs24Hour] = useSettingsHelper('IS_24_HOUR_FORMAT');
-
-  useEffect(() => {
-    const unsub = settings.subscribe((state, prevState) => {
-      if (state.SELECTED_LOCALE !== prevState.SELECTED_LOCALE) {
-        loadLocale(state.SELECTED_LOCALE)
-          .then(() => {
-            I18nManager.forceRTL(isRTL);
-            // allow some time for forceRTL to work
-            setTimeout(restart, 200);
-          })
-          .catch(() => {});
-      }
-    });
-
-    return unsub;
-  });
 
   return (
     <HStack {...props}>
