cfd199e6155dee9c2d80bd519a4d8c5fcd4ea668
Meypod <radical.zero@hotmail.com>
2022-09-25 09:39:05 +0330

    chore: update re-pack to v3

--- index.tsx
+++ index.tsx
@@ -1,4 +1,4 @@
-import {ChunkManager} from '@callstack/repack/dist/client/api/ChunkManager';
+import {ScriptManager, Script} from '@callstack/repack/client';
 
 import {AppRegistry} from 'react-native';
 import {App} from '@/app';
@@ -11,12 +11,25 @@ import {setNextAdhan} from '@/tasks/set_next_adhan';
 import {setUpdateWidgetsAlarms} from '@/tasks/set_update_widgets_alarms';
 import {updateWidgets} from '@/tasks/update_widgets';
 
-ChunkManager.configure({
-  resolveRemoteChunk: async chunkid => {
+ScriptManager.shared.addResolver(async scriptId => {
+  // In development, get all the chunks from dev server.
+  if (__DEV__) {
     return {
-      url: 'file:///' + chunkid,
+      url: Script.getDevServerURL(scriptId),
+      cache: false,
     };
-  },
+  }
+
+  // In production, get chunks matching the regex from filesystem.
+  if (/^.+\.local$/.test(scriptId)) {
+    return {
+      url: Script.getFileSystemURL(scriptId),
+    };
+  } else {
+    return {
+      url: Script.getRemoteURL(`https://my-domain.dev/${scriptId}`),
+    };
+  }
 });
 
 setupNotifeeHandlers();
