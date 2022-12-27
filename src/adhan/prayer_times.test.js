6439a032b983d28fa44f25bfc2b5aa7d73aa559a
Meypod <radical.zero@hotmail.com>
2022-12-27 06:55:12 +0330

    chore: add test for getPrayerTimes

--- src/adhan/prayer_times.test.js
+++ src/adhan/prayer_times.test.js
@@ -1,4 +1,5 @@
-import {isMinimumSettingsAvailable} from '@/adhan';
+import {getPrayerTimes, isMinimumSettingsAvailable} from '@/adhan';
+import {calcSettings} from '@/store/calculation_settings';
 
 const timezones = {
   UTC: 'UTC',
@@ -31,7 +32,45 @@ const tests = timezone =>
     it('timezone change works', () => {
       expect(new Date().toString()).toContain(getTimezoneIdentity(timezone));
     });
-    // it('calculates the same prayer times across all times in a day in the said timezone', () => {});
+
+    describe('getPrayerTimes()', () => {
+      beforeAll(() => {
+        calcSettings.setState({
+          LOCATION_LAT: 1,
+          LOCATION_LONG: 1,
+          CALCULATION_METHOD_KEY: 'MoonsightingCommittee', // doesn't matter which for test
+        });
+      });
+      afterAll(() => {
+        calcSettings.setState({
+          LOCATION_LAT: undefined,
+          LOCATION_LONG: undefined,
+          CALCULATION_METHOD_KEY: undefined,
+        });
+      });
+      it('returns the same prayer for any time inside the same date', () => {
+        const date1 = new Date('Tue Dec 27 2022 00:00:00');
+        const date2 = new Date('Tue Dec 27 2022 00:01:00');
+        const date3 = new Date('Tue Dec 27 2022 12:00:00');
+        const date4 = new Date('Tue Dec 27 2022 23:59:59');
+        const pt1 = getPrayerTimes(date1);
+        const pt2 = getPrayerTimes(date2);
+        const pt3 = getPrayerTimes(date3);
+        const pt4 = getPrayerTimes(date4);
+        delete pt1.date; // as we don't care about the dates comparison
+        delete pt2.date;
+        delete pt3.date;
+        delete pt4.date;
+        expect(pt1).toStrictEqual(pt2);
+        expect(pt1).toStrictEqual(pt3);
+        expect(pt1).toStrictEqual(pt4);
+
+        const date5 = new Date('Tue Dec 28 2022 00:00:00');
+        const pt5 = getPrayerTimes(date5);
+        delete pt5.date;
+        expect(pt5).not.toStrictEqual(pt1);
+      });
+    });
   });
 
 describe('prayer times works properly over different timezones', () => {
