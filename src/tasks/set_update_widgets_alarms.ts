8ab7c7d796a493eb42bc6d7a6685fef7663634da
Meypod <radical.zero@hotmail.com>
2023-01-16 15:46:21 +0330

    fix: revert updating widgets alarm type

--- src/tasks/set_update_widgets_alarms.ts
+++ src/tasks/set_update_widgets_alarms.ts
@@ -6,25 +6,25 @@ import {getNextDayBeginning} from '@/utils/date';
 export function setUpdateWidgetsAlarms() {
   let targetDate = new Date();
 
-  let lastWidgetUpdate = settings.getState().LAST_WIDGET_UPDATE;
+  let {LAST_WIDGET_UPDATE} = settings.getState();
 
   // when the last alarm goes off, it gets past this if
   // because till the last alarm, this condition always returns
   // so that we dont end up setting many alarms
-  if (targetDate.valueOf() < lastWidgetUpdate) return;
+  if (targetDate.valueOf() < LAST_WIDGET_UPDATE) return;
 
   let prayerTimes = getPrayerTimes(targetDate);
 
   if (!prayerTimes) return; // cant get prayer times to set alarms
 
   const begginingOfNextDay = getNextDayBeginning(targetDate).valueOf();
-  lastWidgetUpdate = begginingOfNextDay;
+  LAST_WIDGET_UPDATE = begginingOfNextDay;
   // day change update
   setAlarm({
     timestamp: begginingOfNextDay,
     taskName: 'update_screen_widget_task', // reuse the task name for widget module update request
     allowedInForeground: true,
-    type: 'setExact',
+    type: 'setExactAndAllowWhileIdle',
     wakeup: false,
     keepAwake: false,
   });
@@ -34,14 +34,14 @@ export function setUpdateWidgetsAlarms() {
       timestamp: prayerTimes[prayer].valueOf(),
       taskName: 'update_screen_widget_task',
       allowedInForeground: true,
-      type: 'setExact',
+      type: 'setExactAndAllowWhileIdle',
       wakeup: false,
       keepAwake: false,
     });
-    if (prayerTimes[prayer].valueOf() > lastWidgetUpdate) {
-      lastWidgetUpdate = prayerTimes[prayer].valueOf();
+    if (prayerTimes[prayer].valueOf() > LAST_WIDGET_UPDATE) {
+      LAST_WIDGET_UPDATE = prayerTimes[prayer].valueOf();
     }
   }
 
-  settings.setState({LAST_WIDGET_UPDATE: lastWidgetUpdate});
+  settings.setState({LAST_WIDGET_UPDATE});
 }
