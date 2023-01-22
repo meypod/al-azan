3a582a38d135511faa8d9a9c4f8ad2ebf899812b
Meypod <radical.zero@hotmail.com>
2023-01-22 08:28:15 +0330

    fix: migrate to rn 71.1

--- ios/AlAzan/AppDelegate.mm
+++ ios/AlAzan/AppDelegate.mm
@@ -5,7 +5,11 @@
 @implementation AppDelegate
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  self.moduleName = @"RnDiffApp";
+  self.moduleName = @"AlAzan";
+  // You can add your custom initial props in the dictionary below.
+  // They will be passed down to the ViewController used by React Native.
+  self.initialProps = @{};
+
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
