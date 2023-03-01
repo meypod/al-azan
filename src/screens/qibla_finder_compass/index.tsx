b5f54d10f63b5c6a6116a0b429d4c1be5982d384
Meypod <radical.zero@hotmail.com>
2023-03-01 08:50:25 +0330

    feat: add button for current location in compass

--- src/screens/qibla_finder_compass/index.tsx
+++ src/screens/qibla_finder_compass/index.tsx
@@ -1,12 +1,23 @@
 import {t} from '@lingui/macro';
 import {Qibla, Coordinates} from 'adhan';
-import {Flex, HStack, Input, useColorMode, View, Text} from 'native-base';
-import {useEffect, useMemo, useRef, useState} from 'react';
+import {
+  Flex,
+  HStack,
+  Input,
+  useColorMode,
+  View,
+  Text,
+  Button,
+  Spinner,
+} from 'native-base';
+import {useCallback, useEffect, useMemo, useRef, useState} from 'react';
 import {
   useWindowDimensions,
   Image,
   TextInput as NativeTextInput,
+  ToastAndroid,
 } from 'react-native';
+import LocationProvider from 'react-native-get-location';
 import CompassMod, {
   setCompassLocation,
   setUpdateRate,
@@ -20,6 +31,10 @@ export function QiblaCompass() {
   const {colorMode} = useColorMode();
   const [staticQiblaDegree, setStaticQiblaDegree] = useState(0);
   const qiblaDegree = useRef(0);
+  const [gettingLocation, setGettingLocation] = useState(false);
+  const [fetchedCoords, setFetchedCoords] = useState<
+    {lat: number; long: number} | undefined
+  >(undefined);
 
   const dimensions = useWindowDimensions();
 
@@ -33,22 +48,49 @@ export function QiblaCompass() {
       }
     }, [dimensions.height, dimensions.width]);
 
+  const updateQiblaDegree = useCallback(
+    ({lat, long}: {lat: number; long: number}) => {
+      setCompassLocation(lat, long, 1);
+      const qibla = Qibla(new Coordinates(lat, long));
+      qiblaDegree.current = qibla;
+      setStaticQiblaDegree(Math.round(qiblaDegree.current));
+    },
+    [],
+  );
+
+  const refreshLocation = useCallback(() => {
+    setGettingLocation(true);
+    LocationProvider.getCurrentPosition({
+      enableHighAccuracy: true,
+      timeout: 15000,
+    })
+      .then(location => {
+        setFetchedCoords({lat: location.latitude, long: location.longitude});
+        updateQiblaDegree({lat: location.latitude, long: location.longitude});
+      })
+      .catch(() => {
+        ToastAndroid.show(
+          t`Error while getting coordinates`,
+          ToastAndroid.SHORT,
+        );
+      })
+      .finally(() => setGettingLocation(false));
+  }, [updateQiblaDegree]);
+
   useEffect(() => {
     const {LOCATION_LAT, LOCATION_LONG} = calcSettings.getState();
     if (LOCATION_LAT && LOCATION_LONG) {
-      setCompassLocation(LOCATION_LAT, LOCATION_LONG, 1);
-      const qibla = Qibla(new Coordinates(LOCATION_LAT, LOCATION_LONG));
-      qiblaDegree.current = qibla;
-      setStaticQiblaDegree(Math.round(qiblaDegree.current));
+      setFetchedCoords(undefined);
+      updateQiblaDegree({lat: LOCATION_LAT, long: LOCATION_LONG});
     }
-    setUpdateRate(20);
-  }, []);
+  }, [updateQiblaDegree]);
 
   const compassImgRef = useRef<Image>(null);
   const qiblaImgRef = useRef<Image>(null);
   const degreeTextRef = useRef<NativeTextInput>(null);
 
   useEffect(() => {
+    setUpdateRate(20);
     const sub = CompassMod.addListener('heading', degrees => {
       compassImgRef.current?.setNativeProps({
         transform: [
@@ -73,7 +115,7 @@ export function QiblaCompass() {
 
   return (
     <Flex safeArea py="2" flexDirection="column">
-      <HStack justifyContent="space-between" px="3">
+      <HStack justifyContent="space-between" px="3" mb="4">
         <AccuracyIndicator accuracy={accuracy} />
         <Text>
           {t`Qibla`}
@@ -82,6 +124,31 @@ export function QiblaCompass() {
           {'\u00B0 '}
           {t`from North`}
         </Text>
+      </HStack>
+      <HStack px="3" alignItems="center" justifyContent="space-between">
+        <Text>
+          {t`Location`}
+          {': '}
+          {fetchedCoords
+            ? fetchedCoords.lat.toFixed(2) +
+              ', ' +
+              fetchedCoords.long.toFixed(2)
+            : t`from settings`}
+        </Text>
+        <Button
+          size="sm"
+          variant="outline"
+          onPress={refreshLocation}
+          disabled={gettingLocation}>
+          {gettingLocation ? (
+            <Spinner
+              accessibilityLabel={t`Getting coordinates`}
+              color="lime.200"
+            />
+          ) : (
+            <Text fontSize="xs">{t`Refresh using GPS`}</Text>
+          )}
+        </Button>
       </HStack>
       <Flex
         position="relative"
