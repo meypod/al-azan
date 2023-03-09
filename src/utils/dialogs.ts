65d4a92fd6b6571e6d9a9fc4d2b0d7f127442ab7
Meypod <radical.zero@hotmail.com>
2023-03-09 12:10:48 +0330

    fix: update delete dialog

--- src/utils/dialogs.ts
+++ src/utils/dialogs.ts
@@ -3,19 +3,25 @@ import {Alert} from 'react-native';
 
 export function showDeleteDialog(itemName: string) {
   return new Promise(resolve => {
-    Alert.alert(t`Delete`, t`Are you sure you want to delete "${itemName}" ?`, [
-      {
-        text: t`No`,
-        style: 'cancel',
-        onPress: () => resolve(false),
-      },
-      {
-        text: t`Yes`,
-        onPress: () => {
-          resolve(true);
+    Alert.alert(
+      t`Delete`,
+      itemName
+        ? t`Are you sure you want to delete "${itemName}" ?`
+        : t`Are you sure you want to delete this?`,
+      [
+        {
+          text: t`No`,
+          style: 'cancel',
+          onPress: () => resolve(false),
         },
-        style: 'destructive',
-      },
-    ]);
+        {
+          text: t`Yes`,
+          onPress: () => {
+            resolve(true);
+          },
+          style: 'destructive',
+        },
+      ],
+    );
   });
 }
