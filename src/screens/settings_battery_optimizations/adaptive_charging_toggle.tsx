8c7df23df52f1bd04ad55bbbe7d7df480aa2547e
Meypod <radical.zero@hotmail.com>
2023-04-17 23:18:19 +0330

    fix: make a slight change to enhance auto TR

--- src/screens/settings_battery_optimizations/adaptive_charging_toggle.tsx
+++ src/screens/settings_battery_optimizations/adaptive_charging_toggle.tsx
@@ -23,7 +23,7 @@ const AdaptiveChargingToggleView = memo(function AdaptiveChargingToggleView({
       </FormControl.HelperText>
 
       <HStack justifyContent={'space-between'}>
-        <Text>{t`Use different alarm type?`}</Text>
+        <Text>{t`Use a different alarm type?`}</Text>
         <Switch value={value} onToggle={onToggle} size="lg" />
       </HStack>
     </FormControl>
