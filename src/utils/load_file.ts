9a7544c28e2804b7ee734f5bea7b47a8709d2e86
Meypod <radical.zero@hotmail.com>
2023-06-03 15:19:27 +0330

    fix: bring back brotli compression handling

--- src/utils/load_file.ts
+++ src/utils/load_file.ts
@@ -1,20 +1,43 @@
+import {Buffer} from 'buffer';
+import decompress from 'brotli/decompress';
 import {Image} from 'react-native';
 import {FileSystem, Dirs} from 'react-native-file-access';
 
-export async function loadFile(resourceId: number) {
+async function decompressFileInPlace(path: string) {
+  let content = await FileSystem.readFile(path, 'base64');
+  content = Buffer.from(decompress(Buffer.from(content, 'base64'))).toString(
+    'utf-8',
+  );
+  try {
+    await FileSystem.unlink(path);
+  } catch (e) {
+    console.error(e);
+  }
+  await FileSystem.writeFile(path, content, 'utf8');
+  return content;
+}
+
+export async function loadFile(resourceId: number, compressed = true) {
   const uri = Image.resolveAssetSource(resourceId).uri;
 
   let resourcePath: string | undefined = undefined;
   if (uri.startsWith('http')) {
     resourcePath = Dirs.CacheDir + '/resource-' + resourceId;
-    // reset file every time, because its from the dev server
-    await FileSystem.fetch(uri, {
-      path: resourcePath,
-    });
+    if (!(await FileSystem.exists(resourcePath))) {
+      await FileSystem.fetch(uri, {
+        path: resourcePath,
+      });
+      if (compressed) {
+        await decompressFileInPlace(resourcePath);
+      }
+    }
   } else {
     resourcePath = Dirs.CacheDir + '/' + uri;
     if (!(await FileSystem.exists(resourcePath))) {
       await FileSystem.cpAsset(uri, resourcePath);
+      if (compressed) {
+        await decompressFileInPlace(resourcePath);
+      }
     }
   }
 
