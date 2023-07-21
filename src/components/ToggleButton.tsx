a056aa5fcc9bd5bfe35ea849020db3b3625c785f
Meypod <radical.zero@hotmail.com>
2023-07-21 04:59:11 +0330

    feat: extract toggle button

---
+++ src/components/ToggleButton.tsx
@@ -0,0 +1,26 @@
+import {Button, IButtonProps} from 'native-base';
+import {memo} from 'react';
+
+function ToggleButton({active, ...props}: IButtonProps & {active: boolean}) {
+  return (
+    <Button
+      {...props}
+      alignItems="center"
+      justifyContent="center"
+      variant="outline"
+      borderColor={active ? 'primary.600' : 'muted.400'}
+      _text={{
+        color: active ? 'primary.600' : 'dark.200',
+        _dark: {
+          color: active ? 'primary.400' : 'light.300',
+        },
+      }}
+      _dark={{
+        borderColor: active ? 'primary.400' : 'muted.400',
+      }}>
+      {props.children}
+    </Button>
+  );
+}
+
+export default memo(ToggleButton);
--- src/components/advanced_custom_adhan_toggle.tsx
+++ src/components/advanced_custom_adhan_toggle.tsx
@@ -1,6 +1,7 @@
 import {t} from '@lingui/macro';
-import {Button, IButtonProps} from 'native-base';
+import {IButtonProps} from 'native-base';
 import {useCallback} from 'react';
+import ToggleButton from './ToggleButton';
 import {useSettings} from '@/store/settings';
 
 export function AdvancedCustomAdhanToggle(props: IButtonProps) {
@@ -11,24 +12,8 @@ export function AdvancedCustomAdhanToggle(props: IButtonProps) {
   }, [value, setValue]);
 
   return (
-    <Button
-      {...props}
-      alignItems="center"
-      justifyContent="center"
-      onPress={valueToggle}
-      variant="outline"
-      borderColor={value ? 'primary.600' : 'muted.400'}
-      _text={{
-        color: value ? 'primary.600' : 'dark.200',
-        _dark: {
-          color: value ? 'primary.400' : 'light.300',
-        },
-      }}
-      _dark={{
-        borderColor: value ? 'primary.400' : 'muted.400',
-      }}
-      p="1">
+    <ToggleButton {...props} onPress={valueToggle} active={value} p="1">
       {t`Advanced`}
-    </Button>
+    </ToggleButton>
   );
 }
