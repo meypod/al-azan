c8657192594fb3dd8a7e37ab0c4e4ee7634a3a05
Meypod <radical.zero@hotmail.com>
2023-08-07 07:21:14 +0330

    fix: remove extra logic from VolumeChangeReceiver

--- android/app/src/main/java/com/github/meypod/al_azan/VolumeChangeReceiver.java
+++ android/app/src/main/java/com/github/meypod/al_azan/VolumeChangeReceiver.java
@@ -18,35 +18,9 @@ public class VolumeChangeReceiver extends BroadcastReceiver {
             ContextHolder.setApplicationContext(context.getApplicationContext());
         }
 
-
-        int stream = intent.getIntExtra(
-                "android.media.EXTRA_VOLUME_STREAM_TYPE",
-                AudioManager.STREAM_DTMF // just a value we are not interested in, nothing important
-        );
-
-        if (stream == AudioManager.STREAM_MUSIC) {
-            int newVolume = intent.getIntExtra("android.media.EXTRA_VOLUME_STREAM_VALUE", -1);
-            if (newVolume >= 0) {
-                AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
-                int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
-                double sliderValue = newVolume == 0 ? 0 : newVolume / maxVolume;
-                double scaledVolume = scaleVolume(sliderValue);
-                WritableMap data = Arguments.createMap();
-                data.putDouble("volume", scaledVolume);
-                ReactUtils.startHeadlessTask("volume_changed",
-                        data,
-                        30000,
-                        null);
-            }
-        }
-    }
-
-    // from https://stackoverflow.com/a/17033404/20861939
-    private float scaleVolume(double sliderValue) { // assumes slidervalue is between 0 and 100
-        if (sliderValue == 0) return 0;
-        double logSliderValue = Math.log10(sliderValue / 10);
-        double logMaxSliderValue = Math.log10(10);
-        float scaledVolume = (float) (logSliderValue / logMaxSliderValue);
-        return scaledVolume;
+        ReactUtils.startHeadlessTask("volume_pressed",
+                null,
+                30000,
+                null);
     }
 }
